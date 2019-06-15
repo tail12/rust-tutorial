6b52c507e971dfac98a726d707e5d6975a532ea9
tail12 <icchy.tech@gmail.com>
2019-06-15 18:24:29 +0900

    add: println to all variable

--- variables/src/main.rs
+++ variables/src/main.rs
@@ -13,11 +13,14 @@ fn main() {
     let guess: u32 = "42".parse()
         .expect("Not a number!");
 
+    println!("guess is: {}", guess);
+
     // float
     let fx = 2.0;
     let fy: f32 = 3.0;
+    println!("fx is:{}, fy is:{}", fx, fy);
 
     let tup = ("hoge", 1, 2.1);
     let (str_hoge, one, two_point_one) = tup;
-    println!("{}", str_hoge)
+    println!("stf_hoge is: {}, one is: {}, two_point_one is: {}", str_hoge, one, two_point_one);
 }
