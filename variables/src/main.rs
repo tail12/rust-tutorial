83156ae44e1313ad1b8b1fb94d06aa03ceec4e45
tail12 <icchy.tech@gmail.com>
2019-06-15 18:22:17 +0900

    add: tuple processing

--- variables/src/main.rs
+++ variables/src/main.rs
@@ -8,4 +8,16 @@ fn main() {
     println!("The value of x is: {}", x);
     x = 6;
     println!("THe value of x is: {}", x);
+
+    // data struct
+    let guess: u32 = "42".parse()
+        .expect("Not a number!");
+
+    // float
+    let fx = 2.0;
+    let fy: f32 = 3.0;
+
+    let tup = ("hoge", 1, 2.1);
+    let (str_hoge, one, two_point_one) = tup;
+    println!("{}", str_hoge)
 }
